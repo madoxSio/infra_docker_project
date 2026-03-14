3be01ae00481194a4f987d7fcf8d75a5cd8184e4
mahamadou <mahamadou.gory.sio@gmail.com>
2026-03-14 18:35:29 +0100

    fix: on passe le port de vite en 5173 le port standar pour eviter els conflit entre prod et dev vu que en prod on se retrouve souvent avc du 8080

--- e-commerce-vue-main/frontend/vite.config.js
+++ e-commerce-vue-main/frontend/vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080,
+    port: 5173,
     host: '0.0.0.0',
     cors: true,
     proxy: {
